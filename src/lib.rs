fde410bd013735f8bdd267ef4e2318d6f225f4f1
José Guilherme Vanz <jguilhermevanz@suse.com>
2023-05-17 12:22:00 -0300

    Initial code.
    Initial code implementing the policy to enforce PSA labels in the
    namespaces deployed in the cluster.
    
    Signed-off-by: José Guilherme Vanz <jguilhermevanz@suse.com>

---
+++ src/lib.rs
@@ -0,0 +1,278 @@
+use guest::prelude::*;
+use kubewarden_policy_sdk::wapc_guest as guest;
+
+use k8s_openapi::api::core::v1 as apicore;
+
+extern crate kubewarden_policy_sdk as kubewarden;
+use kubewarden::{protocol_version_guest, request::ValidationRequest, validate_settings};
+
+mod settings;
+use settings::Settings;
+
+#[no_mangle]
+pub extern "C" fn wapc_init() {
+    register_function("validate", validate);
+    register_function("validate_settings", validate_settings::<Settings>);
+    register_function("protocol_version", protocol_version_guest);
+}
+
+fn validate(payload: &[u8]) -> CallResult {
+    let validation_request: ValidationRequest<Settings> = ValidationRequest::new(payload)?;
+
+    match serde_json::from_value::<apicore::Namespace>(validation_request.request.object) {
+        Ok(mut namespace) => {
+            let mut labels = namespace.metadata.labels.clone().unwrap_or_default();
+            if let Some(mode) = validation_request.settings.modes.enforce {
+                labels.insert(
+                    "pod-security.kubernetes.io/enforce".to_string(),
+                    mode.to_string(),
+                );
+            }
+            if let Some(version) = validation_request.settings.modes.enforce_version {
+                labels.insert(
+                    "pod-security.kubernetes.io/enforce-version".to_string(),
+                    version,
+                );
+            }
+
+            if let Some(mode) = validation_request.settings.modes.warn {
+                labels.insert(
+                    "pod-security.kubernetes.io/warn".to_string(),
+                    mode.to_string(),
+                );
+            }
+            if let Some(version) = validation_request.settings.modes.warn_version {
+                labels.insert(
+                    "pod-security.kubernetes.io/warn-version".to_string(),
+                    version,
+                );
+            }
+
+            if let Some(mode) = validation_request.settings.modes.audit {
+                labels.insert(
+                    "pod-security.kubernetes.io/audit".to_string(),
+                    mode.to_string(),
+                );
+            }
+            if let Some(version) = validation_request.settings.modes.audit_version {
+                labels.insert(
+                    "pod-security.kubernetes.io/audit-version".to_string(),
+                    version,
+                );
+            }
+            if labels != namespace.metadata.labels.unwrap_or_default() {
+                namespace.metadata.labels = Some(labels);
+                kubewarden::mutate_request(
+                    serde_json::to_value(namespace).expect("cannot serialize mutated object"),
+                )
+            } else {
+                kubewarden::accept_request()
+            }
+        }
+        Err(_) => kubewarden::accept_request(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use k8s_openapi::apimachinery::pkg::apis::meta::v1::ObjectMeta;
+    use k8s_openapi::Resource;
+    use kubewarden::request::{KubernetesAdmissionRequest, ValidationRequest};
+    use kubewarden_policy_sdk::request::GroupVersionKind;
+    use kubewarden_policy_sdk::response::ValidationResponse;
+    use rstest::rstest;
+    use serde_json::Value;
+    use std::collections::BTreeMap;
+
+    #[rstest]
+    #[case::partial_label_update(
+        Some(settings::Level::Baseline),
+        Some("v1.25".to_string()),
+        None, None, None, None,
+        serde_json::json!({
+            "pod-security.kubernetes.io/enforce": "privileged",
+            "pod-security.kubernetes.io/enforce-version": "latest",
+            "pod-security.kubernetes.io/audit": "baseline",
+            "pod-security.kubernetes.io/audit-version": "v1.26",
+            "pod-security.kubernetes.io/warn": "restricted",
+            "pod-security.kubernetes.io/warn-version": "v1.27"
+        })
+    )]
+    #[case::add_missing_labels(
+        Some(settings::Level::Privileged),
+        Some("latest".to_string()),
+        Some(settings::Level::Baseline),
+        Some("v1.25".to_string()),
+        Some(settings::Level::Restricted),
+        Some("v1.27".to_string()),
+        serde_json::json!({ })
+    )]
+    #[case::update_all_labels(
+        Some(settings::Level::Baseline),
+        Some("v1.25".to_string()),
+        Some(settings::Level::Restricted),
+        Some("v1.27".to_string()),
+        Some(settings::Level::Privileged),
+        Some("latest".to_string()),
+        serde_json::json!({
+            "pod-security.kubernetes.io/enforce": "privileged",
+            "pod-security.kubernetes.io/enforce-version": "latest",
+            "pod-security.kubernetes.io/audit": "baseline",
+            "pod-security.kubernetes.io/audit-version": "v1.26",
+            "pod-security.kubernetes.io/warn": "restricted",
+            "pod-security.kubernetes.io/warn-version": "v1.27"
+        })
+    )]
+    fn mutation_tests(
+        #[case] enforce: Option<settings::Level>,
+        #[case] enforce_version: Option<String>,
+        #[case] audit: Option<settings::Level>,
+        #[case] audit_version: Option<String>,
+        #[case] warn: Option<settings::Level>,
+        #[case] warn_version: Option<String>,
+        #[case] namespace_labels: Value,
+    ) {
+        let original_labels: BTreeMap<String, String> =
+            serde_json::from_value(namespace_labels).expect("cannot deserialize labels");
+        let namespace = apicore::Namespace {
+            metadata: ObjectMeta {
+                labels: Some(original_labels.clone()),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+        let request = KubernetesAdmissionRequest {
+            kind: GroupVersionKind {
+                kind: apicore::Namespace::KIND.to_string(),
+                ..Default::default()
+            },
+            object: serde_json::to_value(namespace).expect("Cannot serialize namespace object"),
+            ..Default::default()
+        };
+        let settings = Settings {
+            modes: crate::settings::Modes {
+                enforce: enforce,
+                enforce_version: enforce_version,
+                audit: audit,
+                audit_version: audit_version,
+                warn: warn,
+                warn_version: warn_version,
+            },
+        };
+        let validation_request = ValidationRequest::<Settings> { settings, request };
+        let payload = serde_json::to_string(&validation_request).expect("Cannot serialize payload");
+
+        let response: ValidationResponse =
+            serde_json::from_slice(&validate(payload.as_bytes()).expect("Validation failed"))
+                .expect("Cannot parse response JSON");
+        assert!(response.accepted);
+        assert!(response.mutated_object.is_some());
+        let mutated_object: apicore::Namespace =
+            serde_json::from_value(response.mutated_object.unwrap())
+                .expect("cannot deserialize mutated object");
+        let labels = mutated_object.metadata.labels.unwrap_or_default();
+
+        let mut expected_labels = original_labels;
+        if let Some(enforce) = validation_request.settings.modes.enforce {
+            expected_labels.insert(
+                "pod-security.kubernetes.io/enforce".to_string(),
+                enforce.to_string(),
+            );
+        }
+        if let Some(enforce_version) = validation_request.settings.modes.enforce_version {
+            expected_labels.insert(
+                "pod-security.kubernetes.io/enforce-version".to_string(),
+                enforce_version,
+            );
+        }
+        if let Some(audit) = validation_request.settings.modes.audit {
+            expected_labels.insert(
+                "pod-security.kubernetes.io/audit".to_string(),
+                audit.to_string(),
+            );
+        }
+        if let Some(audit_version) = validation_request.settings.modes.audit_version {
+            expected_labels.insert(
+                "pod-security.kubernetes.io/audit-version".to_string(),
+                audit_version,
+            );
+        }
+        if let Some(warn) = validation_request.settings.modes.warn {
+            expected_labels.insert(
+                "pod-security.kubernetes.io/warn".to_string(),
+                warn.to_string(),
+            );
+        }
+        if let Some(warn_version) = validation_request.settings.modes.warn_version {
+            expected_labels.insert(
+                "pod-security.kubernetes.io/warn-version".to_string(),
+                warn_version,
+            );
+        }
+        assert_eq!(labels, expected_labels);
+    }
+
+    #[test]
+    fn accept_valid_psa_labels_test() {
+        let mut labels: BTreeMap<String, String> = BTreeMap::new();
+        labels.insert(
+            "pod-security.kubernetes.io/enforce".to_string(),
+            "privileged".to_string(),
+        );
+        labels.insert(
+            "pod-security.kubernetes.io/enforce-version".to_string(),
+            "latest".to_string(),
+        );
+        labels.insert(
+            "pod-security.kubernetes.io/audit".to_string(),
+            "baseline".to_string(),
+        );
+        labels.insert(
+            "pod-security.kubernetes.io/audit-version".to_string(),
+            "v1.25".to_string(),
+        );
+        labels.insert(
+            "pod-security.kubernetes.io/warn".to_string(),
+            "restricted".to_string(),
+        );
+        labels.insert(
+            "pod-security.kubernetes.io/warn-version".to_string(),
+            "v1.27".to_string(),
+        );
+
+        let namespace = apicore::Namespace {
+            metadata: ObjectMeta {
+                labels: Some(labels),
+                ..Default::default()
+            },
+            ..Default::default()
+        };
+        let request = KubernetesAdmissionRequest {
+            kind: GroupVersionKind {
+                kind: apicore::Namespace::KIND.to_string(),
+                ..Default::default()
+            },
+            object: serde_json::to_value(namespace).expect("Cannot serialize namespace object"),
+            ..Default::default()
+        };
+        let settings = Settings {
+            modes: crate::settings::Modes {
+                enforce: Some(settings::Level::Privileged),
+                enforce_version: Some("latest".to_string()),
+                audit: Some(settings::Level::Baseline),
+                audit_version: Some("v1.25".to_string()),
+                warn: Some(settings::Level::Restricted),
+                warn_version: Some("v1.27".to_string()),
+            },
+        };
+        let validation_request = ValidationRequest::<Settings> { settings, request };
+        let payload = serde_json::to_string(&validation_request).expect("Cannot serialize payload");
+
+        let response: ValidationResponse =
+            serde_json::from_slice(&validate(payload.as_bytes()).expect("Validation failed"))
+                .expect("Cannot parse response JSON");
+        assert!(response.accepted);
+        assert!(response.mutated_object.is_none());
+    }
+}
---
+++ src/settings.rs
@@ -0,0 +1,227 @@
+use lazy_static::lazy_static;
+use regex::RegexSet;
+
+use serde::{Deserialize, Serialize};
+
+lazy_static! {
+    // Regex to allow the "latest" version and versions like v<major>.<minor>.
+    // However, minor version cannot start with 0.
+    static ref VERSION_REGEXES: RegexSet = RegexSet::new([r"^latest$", r"^v\d+\.(0|[123456789]\d*)$"]).unwrap();
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[serde(rename_all = "lowercase")]
+pub(crate) enum Level {
+    Privileged,
+    Baseline,
+    Restricted,
+}
+
+impl std::fmt::Display for Level {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Level::Privileged => {
+                write!(f, "privileged")
+            }
+            Level::Baseline => {
+                write!(f, "baseline")
+            }
+            Level::Restricted => {
+                write!(f, "restricted")
+            }
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+#[serde(default, rename_all = "kebab-case", deny_unknown_fields)]
+pub(crate) struct Modes {
+    pub enforce: Option<Level>,
+    pub enforce_version: Option<String>,
+    pub audit: Option<Level>,
+    pub audit_version: Option<String>,
+    pub warn: Option<Level>,
+    pub warn_version: Option<String>,
+}
+
+// Describe the settings your policy expects when
+// loaded by the policy server.
+#[derive(Serialize, Deserialize, Default, Debug)]
+#[serde(default)]
+pub(crate) struct Settings {
+    pub modes: Modes,
+}
+
+impl kubewarden::settings::Validatable for Settings {
+    fn validate(&self) -> Result<(), String> {
+        if self.modes.enforce.is_none() && self.modes.audit.is_none() && self.modes.warn.is_none() {
+            return Err(
+                "At least one of the 'enforce', 'audit' or 'warn' configuration must be defined"
+                    .to_string(),
+            );
+        }
+
+        if let Some(version) = &self.modes.enforce_version {
+            let matches: Vec<_> = VERSION_REGEXES.matches(version).into_iter().collect();
+            if matches.is_empty() {
+                return Err(format!("Version {} is invalid. It must follow the v<major>.<minor> pattern or be 'latest' value", version));
+            }
+            if self.modes.enforce.is_none() {
+                return Err("cannot define enforce version with no enforce mode.".to_string());
+            }
+        }
+        if let Some(version) = &self.modes.audit_version {
+            let matches: Vec<_> = VERSION_REGEXES.matches(version).into_iter().collect();
+            if matches.is_empty() {
+                return Err(format!("Version {} is invalid. It must follow the v<major>.<minor> pattern or be 'latest' value", version));
+            }
+            if self.modes.audit.is_none() {
+                return Err("cannot define audit version with no audit mode.".to_string());
+            }
+        }
+        if let Some(version) = &self.modes.warn_version {
+            let matches: Vec<_> = VERSION_REGEXES.matches(version).into_iter().collect();
+            if matches.is_empty() {
+                return Err(format!("Version {} is invalid. It must follow the v<major>.<minor> pattern or be 'latest' value", version));
+            }
+            if self.modes.warn.is_none() {
+                return Err("cannot define warn version with no warn mode.".to_string());
+            }
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    use kubewarden_policy_sdk::settings::Validatable;
+
+    #[test]
+    fn version_with_no_mode_should_fail_test() {
+        let mut settings = Settings {
+            modes: Modes {
+                enforce: None,
+                enforce_version: Some("1.25.0".to_string()),
+                audit: None,
+                audit_version: None,
+                warn: None,
+                warn_version: None,
+            },
+        };
+        assert!(settings.validate().is_err());
+        settings = Settings {
+            modes: Modes {
+                enforce: None,
+                enforce_version: Some("1.25.0".to_string()),
+                audit: None,
+                audit_version: None,
+                warn: None,
+                warn_version: None,
+            },
+        };
+        assert!(settings.validate().is_err());
+
+        settings = Settings {
+            modes: Modes {
+                enforce: None,
+                enforce_version: None,
+                audit: None,
+                audit_version: Some("1.25.0".to_string()),
+                warn: None,
+                warn_version: None,
+            },
+        };
+        assert!(settings.validate().is_err());
+
+        settings = Settings {
+            modes: Modes {
+                enforce: None,
+                enforce_version: None,
+                audit: None,
+                audit_version: None,
+                warn: None,
+                warn_version: Some("1.25.0".to_string()),
+            },
+        };
+        assert!(settings.validate().is_err());
+    }
+
+    #[test]
+    fn user_must_define_at_least_one_mode_test() {
+        let settings = Settings {
+            modes: Modes {
+                enforce: None,
+                enforce_version: None,
+                audit: None,
+                audit_version: None,
+                warn: None,
+                warn_version: None,
+            },
+        };
+        assert!(settings.validate().is_err());
+    }
+
+    #[rstest]
+    #[case("v1.25", true)]
+    #[case("v1.26", true)]
+    #[case("v1.0", true)]
+    #[case("latest", true)]
+    #[case("v1", false)]
+    #[case("v1.027", false)]
+    #[case("1.0", false)]
+    #[case("1.00", false)]
+    #[case("foo", false)]
+    #[case("1234", false)]
+    fn versions_test(#[case] version: String, #[case] is_valid: bool) {
+        let mut settings = Settings {
+            modes: Modes {
+                enforce: Some(Level::Baseline),
+                enforce_version: Some(version.clone()),
+                audit: Some(Level::Baseline),
+                audit_version: Some(version.clone()),
+                warn: Some(Level::Baseline),
+                warn_version: Some(version.clone()),
+            },
+        };
+        assert_eq!(settings.validate().is_ok(), is_valid);
+
+        settings = Settings {
+            modes: Modes {
+                enforce: Some(Level::Baseline),
+                enforce_version: Some(version.clone()),
+                audit: None,
+                audit_version: None,
+                warn: None,
+                warn_version: None,
+            },
+        };
+        assert_eq!(settings.validate().is_ok(), is_valid);
+
+        settings = Settings {
+            modes: Modes {
+                enforce: None,
+                enforce_version: None,
+                audit: Some(Level::Baseline),
+                audit_version: Some(version.clone()),
+                warn: None,
+                warn_version: None,
+            },
+        };
+        assert_eq!(settings.validate().is_ok(), is_valid);
+
+        settings = Settings {
+            modes: Modes {
+                enforce: None,
+                enforce_version: None,
+                audit: None,
+                audit_version: None,
+                warn: Some(Level::Baseline),
+                warn_version: Some(version.clone()),
+            },
+        };
+        assert_eq!(settings.validate().is_ok(), is_valid);
+    }
+}
